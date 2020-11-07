af73edf549cdc312d3059286445043099947a66b
SatoruItaya <str.itaya@gmail.com>
2020-11-07 16:52:09 +0900

    enable refresh-cb

--- python/lambda_function.py
+++ python/lambda_function.py
@@ -1,6 +1,8 @@
 import os
 import requests
 import fitbit
+import boto3
+from ast import literal_eval
 
 LINE_NOTIFY_TOKEN = os.environ["LINE_NOTIFY_TOKEN"]
 HEADERS = {"Authorization": "Bearer %s" % LINE_NOTIFY_TOKEN}
@@ -8,13 +10,34 @@ URL = "https://notify-api.line.me/api/notify"
 
 CLIENT_ID = os.environ["CLIENT_ID"]
 CLIENT_SECRET = os.environ["CLIENT_SECRET"]
-ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-REFRESH_TOKEN = os.environ["REFRESH_TOKEN"]
+REFRESH_CB_BUCKET_NAME = os.environ["REFRESH_CB_BUCKET_NAME"]
+REFRESH_CB_FILE_NAME = os.environ["REFRESH_CB_FILE_NAME"]
+
+s3 = boto3.resource('s3')
+refresh_cb_bucket = s3.Bucket(REFRESH_CB_BUCKET_NAME)
+tmp_file_name = '/tmp/token.txt'
+
+
+def update_token(token):
+
+    f = open(tmp_file_name, 'w')
+    f.write(str(token))
+    f.close()
+    refresh_cb_bucket.upload_file(tmp_file_name, REFRESH_CB_FILE_NAME)
+
+    return
 
 
 def lambda_handler(event, context):
 
-    authd_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
+    refresh_cb_bucket.download_file(REFRESH_CB_FILE_NAME, tmp_file_name)
+
+    tokens = open(tmp_file_name).read()
+    token_dict = literal_eval(tokens)
+    access_token = token_dict['access_token']
+    refresh_token = token_dict['refresh_token']
+
+    authd_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, access_token=access_token, refresh_token=refresh_token, refresh_cb=update_token)
 
     steps_data = authd_client.time_series('activities/steps', period='1m')
     print(steps_data)
