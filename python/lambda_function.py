6d0d91ea4b6b8c07642a023bfdc3ef6e231ffedb
SatoruItaya <str.itaya@gmail.com>
2020-11-29 17:23:03 +0900

    refactoring

--- python/lambda_function.py
+++ python/lambda_function.py
@@ -37,30 +37,32 @@ def format_steps(steps):
 
 def create_weekly_report(steps_dict):
 
-    two_week_steps_dict = {k: v for k, v in steps_dict.items() if k >= today - datetime.timedelta(days=15)}
-    sorted_weekly_steps = sorted(two_week_steps_dict.items(), key=lambda x: x[0])
-    weekly_steps = 0
-    previous_weekly_steps = 0
+    two_weeks_steps_dict = {k: v for k, v in steps_dict.items() if k >= today - datetime.timedelta(days=15)}
+    sorted_week_steps = sorted(two_weeks_steps_dict.items(), key=lambda x: x[0])
+    week_steps = 0
+    previous_week_steps = 0
 
     weekly_message = '\nWeekly Report\n'
 
     for i in range(7):
-        date = sorted_weekly_steps[7 + i][0].strftime('%m/%d %a')
-        steps = sorted_weekly_steps[7 + i][1]
-        weekly_steps += steps
-        previous_weekly_steps += sorted_weekly_steps[i][1]
+        date = sorted_week_steps[7 + i][0].strftime('%m/%d %a')
+        steps = sorted_week_steps[7 + i][1]
+        week_steps += steps
+        previous_week_steps += sorted_week_steps[i][1]
 
         weekly_message += date + ' ' + format_steps(steps) + ' steps\n'
 
-    avg = round(weekly_steps / 7)
-    # TODO ここ直す とれてない
-    one_week_steps_dict = {k: v for k, v in two_week_steps_dict.items() if k >= today - datetime.timedelta(days=8)}
-    max_date_list = [kv[0].strftime('%m/%d') for kv in one_week_steps_dict.items() if kv[0] >= today - datetime.timedelta(days=8) and kv[1] == max(one_week_steps_dict.values())]
-    min_date_list = [kv[0].strftime('%m/%d') for kv in one_week_steps_dict.items() if kv[0] >= today - datetime.timedelta(days=8) and kv[1] == min(one_week_steps_dict.values())]
+    avg = round(week_steps / 7)
 
-    print(max_date_list)
-    print(min_date_list)
-    weekly_message += '\nTotal: ' + format_steps(weekly_steps) + ' steps ' + '(' + '{:+,}'.format(weekly_steps - previous_weekly_steps) + ')\n'
+    one_week_steps_dict = {k: v for k, v in two_weeks_steps_dict.items() if k >= today - datetime.timedelta(days=8)}
+    max_date_list = [kv[0].strftime('%m/%d') for kv in one_week_steps_dict.items()
+                     if kv[0] >= today - datetime.timedelta(days=8) and kv[1] == max(one_week_steps_dict.values())]
+    min_date_list = [kv[0].strftime('%m/%d') for kv in one_week_steps_dict.items()
+                     if kv[0] >= today - datetime.timedelta(days=8) and kv[1] == min(one_week_steps_dict.values())]
+
+    weekly_message += '\n'
+    weekly_message += 'Total: ' + format_steps(week_steps) + ' steps ' + \
+        '(' + '{:+,}'.format(week_steps - previous_week_steps) + ')\n'
     weekly_message += 'Average: ' + format_steps(avg) + ' steps\n'
     weekly_message += 'Max: ' + ','.join(max_date_list) + '\n'
     weekly_message += 'Min: ' + ','.join(min_date_list) + '\n'
@@ -90,7 +92,8 @@ def lambda_handler(event, context):
     access_token = token_dict['access_token']
     refresh_token = token_dict['refresh_token']
 
-    authd_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, access_token=access_token, refresh_token=refresh_token, refresh_cb=update_token)
+    authd_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET,
+                                 access_token=access_token, refresh_token=refresh_token, refresh_cb=update_token)
 
     yearly_steps_data = authd_client.time_series('activities/steps',
                                                  base_date=datetime.date(today.year, 1, 1), end_date=today - datetime.timedelta(days=1))
@@ -98,12 +101,11 @@ def lambda_handler(event, context):
     for i in yearly_steps_data['activities-steps']:
         i['value'] = int(i['value'])
 
-    # create map {key:date, value:step}
-    lifetime_steps_str_dict = {}
+    # create map {key:date(datetime.datetime), value:step(string)}
     lifetime_steps_date_dict = {}
     lifetime_steps_data = authd_client.time_series('activities/steps', period='max')
+
     for i in lifetime_steps_data['activities-steps'][:-1]:
-        lifetime_steps_str_dict[datetime.datetime.strptime(i['dateTime'], '%Y-%m-%d').strftime('%Y-%m-%d')] = int(i['value'])
         lifetime_steps_date_dict[datetime.datetime.strptime(i['dateTime'], '%Y-%m-%d')] = int(i['value'])
 
     message = ''
