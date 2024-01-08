87506f36fd24375a75f2099bdb0ffb1aac3a1339
Satoru Itaya <str.itaya@gmail.com>
2024-01-08 22:16:53 +0900

    implemente get steps history

--- go/main.go
+++ go/main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 
 	"golang.org/x/oauth2"
 
@@ -21,6 +22,19 @@ type Instances struct {
 	SSMClient *ssm.Client
 	S3Client  *s3.Client
 }
+
+const (
+	LIMIT_DAYS  int = 1095
+	DATE_FORMAT     = "2006-01-02"
+)
+
+var (
+	today               = time.Now().UTC()
+	startDate           = os.Getenv("START_DATE")
+	startDateParse, _   = time.Parse(DATE_FORMAT, startDate)
+	refreshCbBucketName = aws.String(os.Getenv("REFRESH_CB_BUCKET_NAME"))
+	refreshCbFileName   = aws.String(os.Getenv("REFRESH_CB_FILE_NAME_GO"))
+)
 
 func main() {
 	lambda.Start(handler)
@@ -59,11 +73,19 @@ func handler() error {
 		return err
 	}
 
-	userProfile, err := callFitbitAPI(context.TODO(), *newAccessToken)
+	lifetimeStepsDataMap, err := getLifetimeStepsHistory(context.TODO(), *newAccessToken)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("Fitbit API Response: %+v\n", userProfile)
+	fmt.Print(lifetimeStepsDataMap)
+
+	/*
+		activityLogList, err := getActivityLogList(context.TODO(), *newAccessToken, "2024-01-01")
+		if err != nil {
+			return err
+		}
+		fmt.Printf("activityLogList: %+v\n", activityLogList)
+	*/
 
 	return nil
 }
@@ -82,8 +104,8 @@ func (instances *Instances) getParameter(parameterName string) (*string, error) 
 
 func (instances *Instances) getRefreshToken() (*string, error) {
 	getObjectOutput, err := instances.S3Client.GetObject(context.TODO(), &s3.GetObjectInput{
-		Bucket: aws.String(os.Getenv("REFRESH_CB_BUCKET_NAME")),
-		Key:    aws.String(os.Getenv("REFRESH_CB_FILE_NAME_GO")),
+		Bucket: refreshCbBucketName,
+		Key:    refreshCbFileName,
 	})
 	if err != nil {
 		return nil, err
@@ -122,7 +144,7 @@ func (instances *Instances) refreshAccessToken(ctx context.Context, clientID str
 		return nil, err
 	}
 
-	tempFileName := "/tmp/" + os.Getenv("REFRESH_CB_FILE_NAME_GO")
+	tempFileName := "/tmp/" + *refreshCbBucketName
 
 	tempFile, err := os.Create(tempFileName)
 	if err != nil {
@@ -144,8 +166,8 @@ func (instances *Instances) refreshAccessToken(ctx context.Context, clientID str
 	defer file.Close()
 
 	_, err = instances.S3Client.PutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String(os.Getenv("REFRESH_CB_BUCKET_NAME")),
-		Key:    aws.String(os.Getenv("REFRESH_CB_FILE_NAME_GO")),
+		Bucket: refreshCbBucketName,
+		Key:    refreshCbFileName,
 		Body:   file,
 	})
 	if err != nil {
@@ -155,6 +177,96 @@ func (instances *Instances) refreshAccessToken(ctx context.Context, clientID str
 	return &newToken.AccessToken, nil
 }
 
+func getLifetimeStepsHistory(ctx context.Context, access_token string) (map[string]string, error) {
+	// Number of target days
+	restTargetDays := int(today.Sub(startDateParse).Hours() / 24)
+	count := 0
+
+	lifetimeStepsDataMap := map[string]string{}
+
+	for restTargetDays > 0 {
+		tmpEndDate := today.Add(-24 * time.Hour * time.Duration(1+LIMIT_DAYS*count))
+
+		var tmpStartDate time.Time
+		if restTargetDays > LIMIT_DAYS {
+			tmpStartDate = tmpEndDate.Add(-24 * time.Hour * time.Duration(LIMIT_DAYS-1))
+		} else {
+			tmpStartDate = startDateParse
+		}
+
+		tmpStepsData, err := getStepsByDateRange(context.TODO(), access_token, tmpStartDate.Format(DATE_FORMAT), tmpEndDate.Format(DATE_FORMAT))
+		if err != nil {
+			return nil, err
+		}
+
+		for _, dailyHistory := range tmpStepsData["activities-steps"] {
+			lifetimeStepsDataMap[dailyHistory["dateTime"]] = dailyHistory["value"]
+		}
+
+		restTargetDays -= LIMIT_DAYS
+		count += 1
+	}
+
+	return lifetimeStepsDataMap, nil
+}
+
+func getStepsByDateRange(ctx context.Context, access_token string, startDate string, endDate string) (map[string][]map[string]string, error) {
+	api_url := "https://api.fitbit.com/1/user/-/activities/steps/date/" + startDate + "/" + endDate + ".json"
+	client := &http.Client{}
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, api_url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Fitbit API request: %v", err)
+	}
+
+	req.Header.Add("Authorization", "Bearer "+access_token)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to call Fitbit API: %v", err)
+	}
+	defer resp.Body.Close()
+
+	var response_data map[string][]map[string]string
+	if err := json.NewDecoder(resp.Body).Decode(&response_data); err != nil {
+		return nil, fmt.Errorf("failed to decode Fitbit API response: %v", err)
+	}
+
+	return response_data, nil
+}
+
+/*
+func getActivityLogList(ctx context.Context, access_token string, endDate string) (map[string]interface{}, error) {
+	api_url := "https://api.fitbit.com/1/user/-/activities/list.json"
+	client := &http.Client{}
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, api_url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Fitbit API request: %v", err)
+	}
+
+	query := req.URL.Query()
+	query.Set("afterDate", endDate)
+	query.Set("sort", "asc")
+	query.Set("limit", "100")
+	query.Set("offset", "0")
+	req.URL.RawQuery = query.Encode()
+
+	req.Header.Add("Authorization", "Bearer "+access_token)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to call Fitbit API: %v", err)
+	}
+	defer resp.Body.Close()
+
+	var response_data map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&response_data); err != nil {
+		return nil, fmt.Errorf("failed to decode Fitbit API response: %v", err)
+	}
+
+	return response_data, nil
+
+}
+*/
+
+/*
 func callFitbitAPI(ctx context.Context, access_token string) (map[string]interface{}, error) {
 	api_url := "https://api.fitbit.com/1/user/-/profile.json"
 	client := &http.Client{}
@@ -177,3 +289,4 @@ func callFitbitAPI(ctx context.Context, access_token string) (map[string]interfa
 
 	return response_data, nil
 }
+*/
